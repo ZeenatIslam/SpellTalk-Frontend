092beaa2d9c5585dfd8d14e3edd1d0526097b54c
ZeenatIslam <zeenatislam011@gmail.com>
2026-04-20 02:04:29 +0530

    feat:socket.io-client integrated

---
+++ src/pages/Auth.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <div>
+        registeration and login page
+
+        username and password
+        email register
+        add bio
+        profile picture add
+        Welcome to SpellTalk
+      
+    </div>
+  )
+}
+
+export default Auth
